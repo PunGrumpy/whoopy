a99c331a6b175d5ee2f4fe7fe3c5d6ed780861a6
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2026-06-08 18:05:10 +0700

    feat(ui): add all components from shadcn/ui

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -5,5 +5,5 @@ import react from "ultracite/oxlint/react";
 
 export default defineConfig({
   extends: [core, react, next],
-  ignorePatterns: core.ignorePatterns,
+  ignorePatterns: ["apps/web/components/ui"],
 });
